93c136685807fd54202b31a73c13d52e095f3e03
karita <shigekikarita@gmail.com>
2016-11-02 01:44:15 +0900

    update test

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from numpy.distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-# ext = Extension("ddot_cython",
-#                 sources=["ddot_cython.pyx", "ddot_c.c"])
-#
-# setup(
-#     name="ddot_cython",
-#     ext_modules = cythonize([ext])
-# )
-
-
-ext = Extension("svm_wrap", sources=["svm_wrap.pyx", "svm.cpp"], extra_compile_args=["-std=c++11"])
-setup(name="svm_wrap", ext_modules = cythonize([ext]))
+ext = Extension(
+    "svm_wrap",
+    sources=["svm_wrap.pyx", "svm.cpp"],
+    extra_compile_args=["-std=c++11", "-O3"]
+)
+setup(
+    name="svm_wrap",
+    ext_modules = cythonize([ext])
+)
--- test.py
+++ test.py
@@ -17,35 +17,44 @@ def plot(model, X, Y):
     plt.cla()
     xx, yy, xy = surface(X)
     Z = model.predict(xy).reshape(xx.shape)
-    print("predict surface")
     levels = numpy.arange(numpy.min(Z), numpy.max(Z), 1e-2)
-    plt.contourf(xx, yy, Z, levels, cmap=plt.get_cmap("bwr"))
-    print("plot surface")
-
-    # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], s=100, c=Y, cmap=plt.get_cmap("bwr"))
-
-s = svm_wrap.SVMWrapper()
-dtype = numpy.float64
-num = 100
-dim = 2
-xs0 = numpy.random.randn(num, dim).astype(dtype)
-cov = numpy.array([[3.0, 0.0], [0.0, 1.2]])
-xs1 = numpy.random.randn(num, dim).astype(dtype).dot(cov) + numpy.array([3.0, 2.0])
-xs = numpy.concatenate([xs0, xs1])
-ts0 = -numpy.ones(len(xs0))
-ts1 = numpy.ones(len(xs1))
-ts = numpy.concatenate([ts0, ts1])
-s.fit(xs, ts)
+    cmap = plt.get_cmap("bwr")
+    plt.contourf(xx, yy, Z, levels, cmap=cmap)
+    plt.scatter(X[:, 0], X[:, 1], s=100, c=Y, cmap=cmap)
 
 
-success = 0
-for x, t in zip(xs, ts):
-    y = s.predict(x)
-    print("expect %f, actual %f" % (t, y))
-    if numpy.sign(y) == t:
-        success += 1
+def rands(num=10, dim=2, dtype = numpy.float64):
+    return numpy.random.randn(num, dim).astype(dtype)
 
-print("train accuracy: " + str(success / num / 2))
-plot(s, xs, ts)
-plt.show()
+
+def dataset():
+    xs0 = rands()
+    xs1 = rands().dot(numpy.array([[3.0, 0.0], [0.0, 1.2]])) + numpy.array([3.0, 2.0])
+    ts0 = -numpy.ones(len(xs0))
+    ts1 = numpy.ones(len(xs1))
+    xs = numpy.concatenate([xs0, xs1])
+    ts = numpy.concatenate([ts0, ts1])
+    return xs, ts
+
+
+def test():
+    s = svm_wrap.SVMWrapper()
+    xs, ts = dataset()
+    s.fit(xs, ts)
+
+    success = 0.0
+    for x, t in zip(xs, ts):
+        y = s.predict(x)
+        print("expect %f, actual %f" % (t, y))
+        if numpy.sign(y) == t:
+            success += 1.0
+    success /= len(xs)
+
+    print("train accuracy: %f" % success)
+    plot(s, xs, ts)
+    plt.show()
+
+
+# endless joy
+while True:
+    test()
